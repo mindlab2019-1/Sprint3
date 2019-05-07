482edf451c6e23d5fe8ab8b55309480fc9470fe8
cosmo-naut <aidan.tesla@live.com>
2019-05-07 23:55:52 +1000

    Adding annotations and connecting to database

---
+++ MindLab/Assets/Scripts/Annotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Annotation : MonoBehaviour 
+{
+    public Text text;
+
+    public void Initialise(Vector3 vect, String _s)
+    {
+        transform.position = vect;
+        text.text = _s;
+        transform.LookAt(Vector3.zero);
+    }
+}
---
+++ MindLab/Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GvrTrackedController Controller;
+    private float distance = 4f;
+
+    void Start()
+    {
+        AirtableInterface.Instance.GetData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Controller.ControllerInputDevice.GetButtonDown(GvrControllerButton.TouchPadButton))
+        {
+            Vector3 point = Controller.GetComponentInChildren<GvrLaserPointer>().transform.forward.normalized * distance;
+            Debug.Log("Click! Instantiating sphere at "+ point);
+            
+            AnnotationManager.Instance.NewAnnotation(point, "test");
+        }
+    }
+}
